5f6790b4cd171ad0f02b14e8614588bec4aa9a77
mborodin <m.borodin.1999@gmail.com>
2025-09-29 20:22:33 +0200

    add error handling to painting screen

--- lib/ui/core/ui/error_indicator.dart
+++ lib/ui/core/ui/error_indicator.dart
@@ -1,55 +1,43 @@
 import 'package:flutter/material.dart';
 
 class ErrorIndicator extends StatelessWidget {
-  const ErrorIndicator({
-    super.key,
-    required this.title,
-    required this.label,
-    required this.onPressed,
-  });
+  const ErrorIndicator({super.key, required this.title, required this.label});
 
   final String title;
   final String label;
-  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        IntrinsicWidth(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IntrinsicWidth(
             child: Center(
               child: Row(
                 children: [
                   Icon(
                     Icons.error_outline,
-                    color: Theme.of(context).colorScheme.onError,
+                    color: Theme.of(context).colorScheme.error,
                   ),
                   const SizedBox(width: 10),
                   Text(
                     title,
                     style: TextStyle(
-                      color: Theme.of(context).colorScheme.onError,
+                      color: Theme.of(context).colorScheme.error,
                     ),
                   ),
                 ],
               ),
             ),
           ),
-        ),
-        const SizedBox(height: 10),
-        FilledButton(
-          onPressed: onPressed,
-          style: const ButtonStyle(
-            backgroundColor: WidgetStatePropertyAll(Colors.red),
-            foregroundColor: WidgetStatePropertyAll(Colors.white),
+          Text(
+            label,
+            style: TextStyle(color: Theme.of(context).colorScheme.error),
           ),
-          child: Text(label),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
--- lib/ui/paintings/widgets/paintings_screen.dart
+++ lib/ui/paintings/widgets/paintings_screen.dart
@@ -4,6 +4,7 @@ import 'package:twip_app/ui/paintings/view_models/paintings_viewmodel.dart';
 
 import '../../../domain/models/painting/painting.dart';
 import '../../core/localization/applocalization.dart';
+import '../../core/ui/error_indicator.dart';
 import '../../core/ui/navigaton_bar.dart';
 
 const String bookingButtonKey = 'booking-button';
@@ -26,7 +27,15 @@ class _PathScreenState extends State<PaintingsScreen> {
       body: ListenableBuilder(
         listenable: widget.viewModel,
         builder: (context, _) {
+          String? errorMessage = widget.viewModel.errorMessage;
           List<Painting> paintings = widget.viewModel.paintings;
+
+          if (errorMessage != null) {
+            return ErrorIndicator(
+              title: "Error",
+              label: "Can't load the paintings from https://twip.me :("
+            );
+          }
 
           // ListView скролиться
           return CustomScrollView(
