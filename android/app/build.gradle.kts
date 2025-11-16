8f9a5ce12d0ea241a7d52ec5fd46c0342723ddac
mborodin <m.borodin.1999@gmail.com>
2025-11-16 13:41:48 +0100

    Build Android app without signing

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,10 +41,10 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
+//            keyAlias = keystoreProperties["keyAlias"] as String
+//            keyPassword = keystoreProperties["keyPassword"] as String
+//            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+//            storePassword = keystoreProperties["storePassword"] as String
         }
     }
 
@@ -52,7 +52,7 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-//            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("debug")
 //            signingConfig = signingConfigs.getByName("release")
         }
     }
